a5fe1e099d9aa7e3f9319217608fc653f629e547
Fatemeh <aboulhassani.fatemeh@gmail.com>
2024-07-28 21:52:28 +0330

    rename

---
+++ repetition/class2.py
@@ -0,0 +1,14 @@
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+print("Hallo",name,"-","you are",age,"years old")
+
+
+print("-------")
+
+age=int(input("please enter number: "))
+if age >0:
+    print("positive")
+if age<0:
+    print("negative")
+else:
+    print("zero")
